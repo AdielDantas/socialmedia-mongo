0f677866f19d0dc63eb80ea6c0af901d83d63002
Adiel Dantas <adieldantasn@gmail.com>
2025-07-23 11:53:36 -0300

    Inicio do projeto

---
+++ src/main/java/com/devsuperior/socialmedia_mongo/SocialmediaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.socialmedia_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaMongoApplication.class, args);
+	}
+
+}
